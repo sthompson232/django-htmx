f8d2857a5046928806e2f81381f6815d10c60388
Sam Thompson <samthompson292@gmail.com>
2022-06-24 10:03:28 +0100

    fini

--- books/views.py
+++ books/views.py
@@ -1,3 +1,72 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .models import Author, Book
+from .forms import BookForm
+
+
+def index(request):
+  authors = Author.objects.all()
+  context = {
+    'authors': authors
+  }
+  return render(request, 'index.html', context)
+
+
+def create_book(request, pk):
+  author = Author.objects.get(pk=pk)
+  books = Book.objects.filter(author=author)
+  form = BookForm(request.POST or None)
+
+  if request.POST:
+    if form.is_valid():
+      book = form.save(commit=False)
+      book.author = author
+      book.save()
+      return redirect('detail-book', pk=book.pk)
+    else:
+      return render(request, 'partials/book_form.html', { 'form': form })
+
+  context = {
+    'form': form,
+    'author': author,
+    'books': books
+  }
+  return render(request, 'create_book.html', context)
+
+
+def create_book_form(request):
+  context = {
+    'form': BookForm()
+  }
+  return render(request, 'partials/book_form.html', context)
+
+
+def update_book_form(request, pk):
+  book = Book.objects.get(pk=pk)
+  form = BookForm(request.POST or None, instance=book)
+
+  if request.POST:
+    if form.is_valid():
+      book = form.save()
+      return redirect('detail-book', pk=book.pk)
+      
+  context = {
+    'form': form,
+    'book': book
+  }
+  return render(request, 'partials/book_form.html', context)
+
+
+def detail_book(request, pk):
+  book = Book.objects.get(pk=pk)
+  context = {
+    'book': book
+  }
+  return render(request, 'partials/book_detail.html', context)
+
+
+def delete_book(request, pk):
+  book = Book.objects.get(pk=pk)
+  book.delete()
+  return HttpResponse('')
--- djangohtmx/urls.py
+++ djangohtmx/urls.py
@@ -1,21 +1,14 @@
-"""djangohtmx URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
+from books import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('<pk>/', views.create_book, name='create-book'),
+    path('htmx/book/<pk>/detail/', views.detail_book, name='detail-book'),
+    path('html/book/create/', views.create_book_form, name='create-book'),
+    path('htmx/book/<pk>/delete/', views.delete_book, name='delete-book'),
+    path('htmx/book/<pk>/update/', views.update_book_form, name='update-book'),
 ]
